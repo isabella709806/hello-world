ee9f720e216eb711530d8afbe3a23e678258afab
isabella709806 <122543587+isabella709806@users.noreply.github.com>
2023-01-13 09:43:58 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,4 +7,7 @@ input.onButtonPressed(Button.AB, function () {
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Happy)
 })
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
 basic.showIcon(IconNames.Sad)
