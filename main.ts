0da02478d031e4dcaea83da13f4bd99737f57e05
isabella709806 <122543587+isabella709806@users.noreply.github.com>
2023-01-13 09:35:43 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Angry)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.showIcon(IconNames.Sad)
